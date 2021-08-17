9c057f9fd6115fa278b35a3edb1927591f184f8c
SI-YU <bito8651@gmail.com>
2021-08-17 20:06:52 +0800

    calc average

--- read.py
+++ read.py
@@ -6,8 +6,10 @@ with open('reviews.txt', 'r') as f:
         count += 1
         if count % 1000 == 0: # %用來求餘數
             print(len(data)) # print很花時間，所以會讀的比原本沒有按次print還慢很多
-print(len(data))
+print('檔案讀取完了，總共有',len(data), '筆資料')
 
-print(data[0])
-print('--------------')
-print(data[1])
+sum_len = 0
+for d in data: # 將data清單中每一筆取出的字串(留言)稱為d變數
+    sum_len = sum_len + len(d)
+
+print('留言的平均長度為', sum_len/len(data))
