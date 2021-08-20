f65fe55b09af91314521ef524ecd56f0a2da26a6
SI-YU <bito8651@gmail.com>
2021-08-20 23:46:34 +0800

    add word count function

--- read.py
+++ read.py
@@ -1,22 +1,22 @@
-# 求清單中資料筆數
 data = []
 count = 0
 with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
         count += 1
-        if count % 1000 == 0: # %用來求餘數
-            print(len(data)) # print很花時間，所以會讀的比原本沒有按次print還慢很多
+        if count % 1000 == 0:
+            print(len(data))
 print('檔案讀取完了，總共有',len(data), '筆資料')
 
-# 取留言平均長度(字元)
-sum_len = 0
-for d in data: # 將data清單中每一筆取出的字串(留言)稱為d變數
-    sum_len = sum_len + len(d)
+print(data[0])
 
+
+sum_len = 0
+for d in data:
+    sum_len = sum_len + len(d)
 print('留言的平均長度為', sum_len/len(data))
 
-# 篩選
+
 new = []
 for d in data:
     if len(d) < 100:
@@ -25,24 +25,38 @@ print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
 print(new[1])
 
+
 good = []
 for d in data:
-    if 'good' in d: # 如果good有在d字串裡面
-        good.append(d) # 就把該字串加進good清單
+    if 'good' in d:
+        good.append(d)
 print('一共有', len(good), '筆留言提到good')
-print(good[0])
-# list comprehension（清單快寫法）
-good = [d for d in data if 'good' in d] # line 28 to line 31
-print(good)
-# line 28 to line 31 的寫法是把符合條件的留言一個一個append進去list裡面
-# 當把這邊的d替換成1
-good = [1 for d in data if 'good' in d] # line 28 to line 31
-print(good)
-# 以1為例，在跑for loop時，只要符合條件，就把1裝進good清單裡面。
-# 迴圈跑完後，清單裡面會有很多個1，而不是像原本的一則一則留言d。
-# 適用於不需要將原本東西原封不動裝進清單的狀況
-# line 35 的第一個d是指data中的每一個d，就是原本的字串(留言)原封不動裝進去good清單裡面當d裡面包含'good'時
 
-bad = ['bad' in d for d in data]
-# 'bad' in d 是True/False的運算
-print(bad)
+
+# 文字計數
+wc = {} # word_count
+for d in data: # nested for loop(巢狀迴圈)：for loop in fot loop
+    words = d.split() # 裡面不放' '就會預設使用空白鍵（不限字數）切割
+    for word in words:
+        if word in wc:
+            wc[word] += 1
+        else:
+            wc[word] = 1 # 新增新的key進字典
+
+for word in wc:
+    if wc[word] > 1000000:
+        print(word, wc[word])
+
+print(len(wc))
+print(wc['Allen'])
+
+while True:
+    word = input('請問你想查什麼字： ')
+    if word == 'q':
+        break
+    if word in wc:
+        print(word, '出現過的次數為： ', wc[word])
+    else:
+        print('這個字沒有出現過喔！')
+
+print('感謝使用本查詢功能')
